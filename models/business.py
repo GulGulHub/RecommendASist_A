7e43f9850cf261573ab492ee5aa1a63085a141bc
GulGulHub <guelden.ediger@gmx.net>
2023-02-25 21:51:32 +0100

    added models for classes and a folder for the database files

---
+++ models/business.py
@@ -0,0 +1,14 @@
+class Business:
+    def __init__(self, id, name, contact):
+        self.id = id
+        self.name = name
+        self.contact = contact
+        self.info = []
+
+    def add_buisness_to_map(self):
+        # add to API
+
+    def update_info(self):
+        # new_update = get text from form append info list
+
+
---
+++ models/user.py
@@ -0,0 +1,25 @@
+class User:
+
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+        self.is_logged_in = False
+        self.image = []
+
+
+    def sign_up(self):
+        # create database and add all info
+
+
+    def log_in(self):
+        # compare with db and see if log in is successfull, then is_logged in = True
+
+    def recommend_user(self):
+        # add a user
+
+    def add_buisness(self):
+        # add a buisness
+
+
